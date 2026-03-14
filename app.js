b09f856409ce5023b295e2547300771432feda6a
MelissaAdorni <nopie3@gmail.com>
2026-03-14 17:56:41 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const http = require('http');
+const fs = require('fs'); // This "fs" module lets Node read files
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello from the Bubbleverse!\n');
+  // Read our new index.html file
+  fs.readFile('index.html', (err, data) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html'); // Changed from text/plain to text/html
+    res.end(data);
+  });
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Bubbleverse running at http://${hostname}:${port}/`);
 });
